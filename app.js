bcd44305b33b5f2667df63ed73b81216ad0f66f4
maniyadg <maniyarasandhanasekaran@gmail.com>
2023-01-05 15:22:00 +0530

    app added

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "*",
+  origin: "https://capstone-server-xp65.onrender.com",
 }))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
